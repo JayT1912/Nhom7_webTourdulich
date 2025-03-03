58bcb97f8296081c95e0295b9206d31c4e11c3bc
letuankiet2004tn@gmail.com <letuankiet2004tn@gmail.com>
2025-03-03 13:25:28 +0700

    C1

--- Nhom7_webTourdulich/obj/Debug/net9.0/Nhom7_webTourdulich.AssemblyInfo.cs
+++ Nhom7_webTourdulich/obj/Debug/net9.0/Nhom7_webTourdulich.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nhom7_webTourdulich")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538b85e702a7f7f2d0b6e2e1cd51604bab237d09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a733005622e510790757dfe2eb894087cb7f878")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nhom7_webTourdulich")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nhom7_webTourdulich")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
